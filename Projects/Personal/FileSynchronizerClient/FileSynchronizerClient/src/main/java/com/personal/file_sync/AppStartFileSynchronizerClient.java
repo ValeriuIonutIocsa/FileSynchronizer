824592cf55e1fe125767d7d3866f921a8ddfdf86
Iocsa, Valeriu-Ionut <valeriu.iocsa@vitesco.com>
2025-04-09 01:00:15 +0300

    updated dependencies

--- Projects/Personal/FileSynchronizerClient/FileSynchronizerClient/src/main/java/com/personal/file_sync/AppStartFileSynchronizerClient.java
+++ Projects/Personal/FileSynchronizerClient/FileSynchronizerClient/src/main/java/com/personal/file_sync/AppStartFileSynchronizerClient.java
@@ -5,6 +5,10 @@ import java.time.Instant;
 import com.personal.file_sync.dist.FileSynchronizerHttpClient;
 import com.personal.file_sync.settings.FactoryFileSynchronizerClientSettings;
 import com.personal.file_sync.settings.FileSynchronizerClientSettings;
+import com.utils.io.file_deleters.FactoryFileDeleter;
+import com.utils.io.file_deleters.FileDeleterWin;
+import com.utils.io.folder_deleters.FactoryFolderDeleter;
+import com.utils.io.folder_deleters.FolderDeleterWin;
 import com.utils.log.Logger;
 import com.utils.log.progress.ProgressIndicatorConsole;
 import com.utils.log.progress.ProgressIndicators;
@@ -19,6 +23,9 @@ final class AppStartFileSynchronizerClient {
 
 		final Instant start = Instant.now();
 		ProgressIndicators.setInstance(ProgressIndicatorConsole.INSTANCE);
+
+		FactoryFolderDeleter.setInstance(new FolderDeleterWin());
+		FactoryFileDeleter.setInstance(new FileDeleterWin());
 
 		final FileSynchronizerClientSettings fileSynchronizerClientSettings =
 				FactoryFileSynchronizerClientSettings.newInstance(args);
--- Projects/Personal/FileSynchronizerServer/FileSynchronizerServer/src/main/java/com/personal/file_sync/AppStartFileSynchronizerServer.java
+++ Projects/Personal/FileSynchronizerServer/FileSynchronizerServer/src/main/java/com/personal/file_sync/AppStartFileSynchronizerServer.java
@@ -3,6 +3,10 @@ package com.personal.file_sync;
 import com.personal.file_sync.dist.FileSynchronizerHttpServer;
 import com.personal.file_sync.settings.FactoryFileSynchronizerServerSettings;
 import com.personal.file_sync.settings.FileSynchronizerServerSettings;
+import com.utils.io.file_deleters.FactoryFileDeleter;
+import com.utils.io.file_deleters.FileDeleterWin;
+import com.utils.io.folder_deleters.FactoryFolderDeleter;
+import com.utils.io.folder_deleters.FolderDeleterWin;
 import com.utils.log.progress.ProgressIndicatorConsole;
 import com.utils.log.progress.ProgressIndicators;
 
@@ -15,6 +19,9 @@ final class AppStartFileSynchronizerServer {
 			final String[] args) {
 
 		ProgressIndicators.setInstance(ProgressIndicatorConsole.INSTANCE);
+
+		FactoryFolderDeleter.setInstance(new FolderDeleterWin());
+		FactoryFileDeleter.setInstance(new FileDeleterWin());
 
 		final FileSynchronizerServerSettings fileSynchronizerServerSettings =
 				FactoryFileSynchronizerServerSettings.newInstance(args);
